7d01171cc47b26d4c3057178e7dae6e16dfba1db
Iban <iban.etche@outlook.fr>
2022-03-27 18:58:44 +0200

    first commit

---
+++ dist/_nuxt/static/1648400262/portfolio/apibook/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648400262",layout:"default",error:a,serverRendered:b,routePath:"\u002Fportfolio\u002Fapibook",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a3017573"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ dist/_nuxt/static/1648400262/portfolio/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/portfolio", (function(a,b,c,d){return {data:[{articles:[{slug:a,description:b,title:c,img:d},{slug:a,description:b,title:c,img:d},{slug:a,description:b,title:c,img:d}]}],fetch:{},mutations:void 0}}("apibook","Application web en Symfony pour les apiculteurs !","Apibook","apibook-logo.png")));
